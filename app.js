22713d8ae27e545e74708d588bcd10df4446a2fd
GoldAbsolute <sokoldima1998@mail.ru>
2022-12-22 17:47:38 +0300

    Добавление и удаление комментариев(фронт)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const App = {
         return {
             apiUrlGet: "http://" + window.location.host + "/api/get",
             apiUrlPost: "http://" + window.location.host + "/api/post",
+            apiUrlDelete: "http://" + window.location.host + "api/delete/",
             comments: [],
             errors: [],
             form: {
@@ -19,6 +20,7 @@ const App = {
         //
         console.log(this.apiUrlGet)
         console.log(this.apiUrlPost)
+        console.log(this.apiUrlDelete)
         //
         this.getComments()
         //
@@ -46,6 +48,65 @@ const App = {
                 this.errors.push(error);
                 console.log(this.errors)
             }
+        },
+        async addComment() {
+            if (this.captcha == this.secretCaptcha) {
+                try {
+                    console.log("Отправка комментария")
+                    console.log(this.form)
+                    const result = await fetch(this.apiUrlPost, {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            name: this.form.name,
+                            text: this.form.text
+                        }),
+                        headers: {
+                            'Content-type': 'application/json; charset=utf-8',
+                        },
+                        credentials: "include",
+                    })
+                    const data = await result.json()
+                    console.log(data)
+                } catch (error) {
+                    console.error(error)
+                }
+                this.form.name = '';
+                this.form.text = '';
+                await this.updateComments()
+                //
+                this.captcha = ''
+                this.captchaGenerate()
+                this.captchaGenerateUrl()
+            } else {
+                this.errorClasses='captcha-error'
+                setTimeout(this.hideErrorCaptcha, 20000)
+                // Update captcha
+                this.captcha = ''
+                this.captchaGenerate()
+                this.captchaGenerateUrl()
+            }
+
+        },
+        async deleteComment(comment) {
+            try {
+                console.log("Удаление комментария")
+                console.log(comment.id)
+                const result = await fetch(this.apiUrlDelete+comment.id, {
+                    method: 'DELETE',
+                    body: JSON.stringify({
+                        id: comment.id
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8',
+                    },
+                    credentials: "include",
+                })
+                const data = await result.json()
+                console.log(data)
+            } catch (error) {
+                console.error(error)
+            }
+            await this.updateComments()
         },
     }
 }
